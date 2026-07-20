70bd5d23e84887452dc96f68998e52dcb497fa96
WoodleySolutions <tyler.woodleysolutions@gmail.com>
2026-07-20 16:42:22 -0500

    Handle double letter  Right Letter, Wrong Place guesses

--- Application/Wordle.cs
+++ Application/Wordle.cs
@@ -2,13 +2,15 @@ namespace Application;
 
 public class Wordle
 {
-    private char green = 'G';
-    private char yellow = 'Y';
-    private char gray = '-';
+    private readonly char green = 'G';
+    private readonly char yellow = 'Y';
+    private readonly char gray = '-';
 
     public string Guess(string answer, string guess)
     {
         char[] result = [gray, gray, gray, gray, gray];
+
+        List<char> usedChars = new();
 
         for (int i = 0; i < answer.Length; i++)
         {
@@ -16,8 +18,12 @@ public class Wordle
             {
                 result[i] = green;
             }
+            else if (answer.Contains(guess[i]) && !usedChars.Contains(guess[i]))
+            {
+                result[i] = yellow;
+                usedChars.Add(guess[i]);
+            }
         }
-
 
         return new string(result);
     }
--- UnitTests/WordleEngineTests.cs
+++ UnitTests/WordleEngineTests.cs
@@ -6,10 +6,12 @@ public class WordleEngineTests
 {
     private readonly Wordle _wordle = new Wordle();
 
-    [Theory]
+    [Theory] //Answer, Guess, Expected
     [InlineData("PLANE", "CHART", "--G--")]
     [InlineData("PLANE", "PLANE", "GGGGG")]
     [InlineData("PLANE", "DITCH", "-----")]
+    [InlineData("PLANE", "WORLD", "---Y-")]
+    [InlineData("PLANE", "LEVER", "YY---")]
     public void WordleGuess_ReturnsExpectedResult(string answer, string guess, string expected)
     {
         Assert.Equal(expected, _wordle.Guess(answer, guess));
